c50c07b40c5b6a2d2a215b57b4d2340876b3fdf9
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-08-05 08:52:04 +0700

    Initial Commit

---
+++ RMIT Casino Live Coding/RMIT_Casino_Live_CodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_Casino_Live_CodingApp.swift
+//  RMIT Casino Live Coding
+//
+//  Created by Tom Huynh on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_Casino_Live_CodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
